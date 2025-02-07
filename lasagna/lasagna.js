5e9e24922ae336156da747e0ca3e045253824b24
Usheninte Dangana <usheninte234@gmail.com>
2025-02-07 11:55:30 +0100

    Exported expected minutes in oven variable

--- lasagna/lasagna.js
+++ lasagna/lasagna.js
@@ -34,7 +34,11 @@
  * The number of minutes it takes to prepare a single layer.
  */
 const PREPARATION_MINUTES_PER_LAYER = 2;
-const EXPECTED_MINUTES_IN_OVEN = 40;
+
+/**
+ * The expected number of minutes it takes to prepare a meal.
+ */
+export const EXPECTED_MINUTES_IN_OVEN = 40;
 
 /**
  * Determines the number of minutes the lasagna still needs to remain in the
