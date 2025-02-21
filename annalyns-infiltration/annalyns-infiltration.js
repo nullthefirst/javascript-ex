ba6ddceaadadd69db9ed935bb5b6e07b0307e5dc
Usheninte Dangana <usheninte234@gmail.com>
2025-02-21 12:40:14 +0100

    Completed can signal prisoner task

--- annalyns-infiltration/annalyns-infiltration.js
+++ annalyns-infiltration/annalyns-infiltration.js
@@ -52,7 +52,7 @@ export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
  * @returns {boolean} Whether or not you can send a signal to the prisoner.
  */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-  throw new Error("Remove this line and implement the function");
+  return prisonerIsAwake && !archerIsAwake ? true : false;
 }
 
 /**
